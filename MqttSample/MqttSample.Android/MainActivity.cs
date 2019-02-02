86f4450620a5b2a925f0a6ad9add62cb6a603bce
Jeff Albrecht <jeffa@appdevpro.com>
2019-02-02 14:31:37 -0700

    Add images to Android. Enhance Android MainActiviey.cs for xam esentials. Attempt to resolvePdb2Mdb.convert problem with <DebugType>full</DebugType>   <DebugSymbols>true</DebugSymbols> in MqttSample.csproj

--- MqttSample/MqttSample.Android/MainActivity.cs
+++ MqttSample/MqttSample.Android/MainActivity.cs
@@ -1,6 +1,7 @@
 ﻿using Android.App;
 using Android.Content.PM;
 using Android.OS;
+using Android.Runtime;
 using Prism;
 using Prism.Ioc;
 
@@ -21,7 +22,15 @@ namespace MqttSample.Droid
             global::Xamarin.Forms.Forms.Init(this, bundle);
             LoadApplication(new App(new AndroidInitializer()));
         }
+        public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
+        {
+            Xamarin.Essentials.Platform.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+
+            base.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+        }
     }
+
+
 
     public class AndroidInitializer : IPlatformInitializer
     {
