5d2ea39a4f512ecd9d8f73fba3b55e2138548d57
Jeff Albrecht <jeffa@appdevpro.com>
2019-02-21 12:53:08 -0700

    Create a readme for RepoDates.ConsoleApp

--- RepoDates.ConsoleApp/Program.cs
+++ RepoDates.ConsoleApp/Program.cs
@@ -20,21 +20,17 @@ namespace RepoDates.ConsoleApp
         static async Task MainAsync(string[] args)
         {
             /*
+                Why did I write this? I just needed some encouragment by completing a task in visual studio.
+                I'd been working on trying to learn how to TLS secure a XamarinForms app for 24 days.
+                I was getting tired, cranky, frustrated for not being able to find just the right docs to solve it myself ....
+
+                Now in case I want to complain, moan or wavie my hands around for perhaps a "There, there", but
+                probably a "RTFM bunky!" on twitter I could quickly calculate how long I've been working on this! :-)
             
-            Why did I write this? I just needed some encouragment by completing a taskk in visual studio.
-            I'd been working on trying to learn how to TLS secure a XamarinForms app for 24 days.
-            I was getting tired, cranky, frustrated for not being able to find just the right docs or solve it myself ....
-
-            Now in case I want to complain, moan or wavie my hands around for perhaps a "There, there", but
-            probably a "RTFM bunky!" on twitter I could quickly calculate how long I've been working on this! :-)
-            
-            This code will display create date and age of a GitHub repository.
-            change these to your liking;
-            string repoOwner = "jhalbrecht";                Your/any GitHub account.
-            string repo = "XamarinFormsMqttSample";         Your/any Repository.
-
-            A good excuse to work with Visual Studio Preview too.
-
+                This code will display create date and age of a GitHub repository.
+                change these to your liking;
+                string repoOwner = "jhalbrecht";                Your/any GitHub account.
+                string repo = "XamarinFormsMqttSample";         Your/any Repository.
             */
 
             Console.WriteLine("Hello RepoDates World!");
---
+++ RepoDates.ConsoleApp/ReadMe.md
@@ -0,0 +1,20 @@
+﻿# RepoDates.ConsoleApp
+Calculate the number of days since a Github Repository has been created.
+
+
+## Why did I write this?
+
+I just needed some encouragment by completing a task in visual studio.
+I'd been working on trying to learn how to TLS secure a XamarinForms app for 24 days.
+I was getting tired, cranky, frustrated for not being able to find just the right docs to solve it myself ....
+At the time of this readme creation I'm still scratching my head.
+
+## Social Media
+Now in case I want to complain, moan or wavie my hands around for perhaps a "There, there", but
+probably a "RTFM bunky!" on twitter I could quickly calculate how long I've been working on this! :-)
+            
+This code will display create date and age of a GitHub repository.
+## How to use
+change these to your liking;
+* string repoOwner = "jhalbrecht";                Your/any GitHub account.
+* string repo = "XamarinFormsMqttSample";         Your/any Repository.
